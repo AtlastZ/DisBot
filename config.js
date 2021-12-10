2df5f696071a1a8cb319c5c29a4f27f90c2e3920
AnalyzyA <analyzya@gmail.com>
2021-12-10 12:41:58 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    app: {
+        px: '!',
+        token: 'OTE4NzI4MDcwMjUxMzE1MjEw.YbLeMQ.qj6a3oQOFQGsUkDC0IlTfIM9mt0',
+        playing: 'by Zerio ❤️'
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: 'DJ',
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
+        },
+        maxVol: 100,
+        loopMessage: false,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
