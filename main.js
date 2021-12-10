5c5936acd5897aef3475eff48462b662dfde87a2
AnalyzyA <analyzya@gmail.com>
2021-12-10 13:55:55 +0700

    main

--- main.js
+++ main.js
@@ -11,11 +11,12 @@ global.client = new Client({
     disableMentions: 'everyone',
 });
 
-client.config = require('./config.js');
+client.config = require('./config');
 
 global.player = new Player(client, client.config.opt.discordPlayer);
 
 require('./src/loader');
 require('./src/events');
 
+// client.login(client.config.app.token);
 client.login(process.env.token);
